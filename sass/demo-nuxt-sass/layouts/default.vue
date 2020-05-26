a8c2a3b8be2f5aed8f5f938c11da36fb355b4c72
Álex Martín <alex@alexdw.com>
2020-05-26 17:25:01 +0200

    Nuxt and SCSS

---
+++ sass/demo-nuxt-sass/layouts/default.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <nuxt />
+  </div>
+</template>
+
+<style lang="scss">
+
+@import "../assets/scss/variables";
+
+html {
+  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
+    Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
+
+*,
+*:before,
+*:after {
+  box-sizing: border-box;
+  margin: 0;
+}
+
+.button--green {
+  display: inline-block;
+  border-radius: 4px;
+  border: 1px solid $font-color;
+  color: $font-color;
+  text-decoration: none;
+  padding: 10px 30px;
+}
+
+.button--green:hover {
+  color: #fff;
+  background-color: #3b8070;
+}
+
+.button--grey {
+  display: inline-block;
+  border-radius: 4px;
+  border: 1px solid $font-color;
+  color: $font-color;
+  text-decoration: none;
+  padding: 10px 30px;
+  margin-left: 15px;
+}
+
+.button--grey:hover {
+  color: #fff;
+  background-color: #35495e;
+}
+</style>
---
+++ sass/demo-nuxt-sass/pages/index.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="container">
+    <div>
+      <h1 class="title">
+        Uso de SCSS en Nuxt
+      </h1>
+      <h2 class="subtitle">
+        Asegúrate de tener las dependencias node-sass y sass-loader
+      </h2>
+      <p>Puedes configurar los scss tanto en nuxt-config como en las páginas, componentes y layouts</p>
+      <p>Las fuentes también las configuras en nuxt-config (en el apartado de head>link)</p>
+      <div class="links">
+        <a
+          href="https://nuxtjs.org/api/configuration-css/"
+          target="_blank"
+          class="button--green"
+        >
+          Info sobre SCSS y Nuxt
+        </a>
+        <a
+          href="https://nuxtjs.org/faq/"
+          target="_blank"
+          class="button--grey"
+        >
+          Uso de fuentes externas
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+
+export default {
+  components: {
+    Logo
+  }
+}
+</script>
+
+<style lang="scss">
+
+@import "../assets/scss/variables";
+
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: $font-title;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: $font-color;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: $font-color;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
